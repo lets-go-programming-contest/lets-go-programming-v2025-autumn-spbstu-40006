a1eb3999d37632b1dc76d3bb442d580f6de57863
Аслямов Марат <marat.aslyamov@gmail.com>
2025-09-19 14:51:19 +0300

    [TASK-1]

---
+++ marat.aslyamov/task-1/cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, result float32
+	var j int
+	var sign string
+	_, err := fmt.Scan(&i)
+	if (err != nil) {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&j)
+	if (err != nil) {
+		fmt.Println("Invalind second operand")
+		return
+	}
+	fmt.Scan(&sign)
+	
+	switch sign {
+	case "+":
+		result = i + j
+	case "-":
+		result = i - j
+	case "*":
+		result = i * j
+	case "/":
+		if (j == 0) {
+			fmt.Println("Division by zero")
+			return
+		}
+		result = i / j
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+	fmt.Println(result)
+}
