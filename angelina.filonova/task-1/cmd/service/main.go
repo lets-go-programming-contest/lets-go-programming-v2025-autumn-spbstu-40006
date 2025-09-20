c35f2b1bfa8bb2130ea3767b7fe68dc194a3bbda
filonova.angelina <filonova2906@yandex.ru>
2025-09-21 00:11:50 +0300

    [TASK-1] add error

--- angelina.filonova/task-1/cmd/service/main.go
+++ angelina.filonova/task-1/cmd/service/main.go
@@ -1,38 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrorDivisionByZero = errors.New("Division by zero")
+	ErrorInvalidOperation = errors.New("Invalid operation")
+	ErrorInvalidFirstOperand = errors.New("Invalid first operand")
+	ErrorInvalidSecondOperand = errors.New("Invalid second operand")
+)
 
 func add(a int, b int) int {
-	return a + b;
+	return a + b
 }
 
 func sub(a int, b int) int {
-	return a - b;
+	return a - b
 }
 
 func multiply(a int, b int) int {
-	return a * b;
+	return a * b
 }
 
-func divide(a int, b int) int {
-	return a / b;
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, ErrorDivisionByZero
+	}
+	return a / b, nil
 }
 
 func main() {
-
 	var num1, num2 int
 	var operand string
 	fmt.Scan(&num1, &num2, &operand)
 
+	var result int
+	var err error 
+
 	switch operand {
 	case "+":
-		add(num1, num2)
-	 case "-":
-        sub(num1, num2)
-    case "*":
-        multiply(num1, num2)
-    case "/":
-        divide(num1, num2)
-
+		result = add(num1, num2)
+	case "-":
+		result = sub(num1, num2)
+	case "*":
+		result = multiply(num1, num2)
+	case "/":
+		result, err = divide(num1, num2) 
+	default:
+		err = ErrorInvalidOperation
 	}
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
 }
