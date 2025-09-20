79c01925a789c03ad9364de91934d7357aac0a84
MoneyprogerISG <dakjacquesnoname@gmail.com>
2025-09-20 11:35:37 +0300

    [TASK-1] (#7)
    * [TASK-1] Initial commit.
    
    * [TASK-1] Fix review comments: reuse err var, group vars, fix module name, rename service to calculator
    
    * [TASK-1] Revert folder name to 'service' for CI compatibility and fix code style
    
    * [TASK-1] Fix code formatting
    
    ---------
    
    Co-authored-by: Груздев <gruzdev_i06@mail.ru>

---
+++ ivan.gruzdev/task-1/cmd/service/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		value1, value2 int
+		operation      string
+	)
+
+	_, err := fmt.Scan(&value1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&value2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(value1 + value2)
+	case "-":
+		fmt.Println(value1 - value2)
+	case "*":
+		fmt.Println(value1 * value2)
+	case "/":
+		if value2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(value1 / value2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
