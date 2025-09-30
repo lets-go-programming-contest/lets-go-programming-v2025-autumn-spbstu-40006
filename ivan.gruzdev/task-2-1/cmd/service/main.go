8dd304b74717b4d070780e5ffffa932c785bdd2a
Груздев <gruzdev_i06@mail.ru>
2025-09-30 16:15:51 +0300

    [TASK-2-1] Initial commit

---
+++ ivan.gruzdev/task-2-1/cmd/service/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func mustScan(a ...interface{}) {
+	_, err := fmt.Scan(a...)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func main() {
+	var (
+		minTemp, maxTemp                         int = 15, 30
+		countDepartment, countStaff, temperature int
+		operation                                string
+	)
+
+	mustScan(&countDepartment)
+
+	for i := 0; i < countDepartment; i++ {
+		mustScan(&countStaff)
+
+		minTemp, maxTemp = 15, 30
+
+		for j := 0; j < countStaff; j++ {
+			mustScan(&operation, &temperature)
+
+			if operation == ">=" {
+				if temperature > minTemp {
+					minTemp = temperature
+				}
+			} else {
+				if temperature < maxTemp {
+					maxTemp = temperature
+				}
+			}
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemp)
+			}
+		}
+	}
+
+}
