7970b10fce37869e1aea1683f545be751a3d4aaf
ivanignashin <ivanignashin@mail.ru>
2025-09-30 20:08:27 +0300

    [TASK-2-2] fix lint problems

--- ivan.ignashin/task-2-2/cmd/service/main.go
+++ ivan.ignashin/task-2-2/cmd/service/main.go
@@ -7,15 +7,16 @@ import (
 
 type MaxHeap []int
 
-func (h MaxHeap) Len() int           { return len(h) }
-func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *MaxHeap) Len() int           { return len(*h) }
+func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *MaxHeap) Push(x interface{}) {
 	value, ok := x.(int)
 	if !ok {
 		return
 	}
+
 	*h = append(*h, value)
 }
 
