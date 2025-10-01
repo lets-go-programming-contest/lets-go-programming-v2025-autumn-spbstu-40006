f58971aed7ff5bfa30c8b91ce1a0ff211bef4054
Segfault-chan <hermanpuzikov2004@gmail.com>
2025-10-01 11:08:01 +0300

    [TASK-1] Fixed version

--- herman.puzikov/task-1/cmd/service/main.go
+++ herman.puzikov/task-1/cmd/service/main.go
@@ -3,51 +3,47 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 var (
-	ErrorDivisionByZero       = errors.New("Division by zero")
-	ErrorInvalidOperation     = errors.New("Invalid operation")
-	ErrorInvalidFirstOperand  = errors.New("Invalid first operand")
-	ErrorInvalidSecondOperand = errors.New("Invalid second operand")
+	ErrorDivisionByZero   = errors.New("division by zero")
+	ErrorInvalidOperation = errors.New("invalid operation")
 )
 
-func main() {
-	var fOp, sOp int
-	var operand string
-	var functionsMap = map[string]func(int, int) (float64, error){
-		"+": func(a, b int) (float64, error) { return float64(a + b), nil },
-		"-": func(a, b int) (float64, error) { return float64(a - b), nil },
-		"*": func(a, b int) (float64, error) { return float64(a * b), nil },
-		"/": func(a, b int) (float64, error) {
+func calculate(operator string, a, b int) (float64, error) {
+	switch operator {
+	case "+":
+		return float64(a + b), nil
+	case "-":
+		return float64(a - b), nil
+	case "*":
+		return float64(a * b), nil
+	case "/":
+		{
 			if b == 0 {
 				return 0, ErrorDivisionByZero
 			}
 			return float64(a) / float64(b), nil
-		},
+		}
+	default:
+		return 0, ErrorInvalidOperation
 	}
+}
 
-	if _, err := fmt.Scan(&fOp); err != nil {
-		fmt.Println(ErrorInvalidFirstOperand)
-		return
-	}
-	if _, err := fmt.Scan(&sOp); err != nil {
-		fmt.Println(ErrorInvalidSecondOperand)
-		return
-	}
-	if _, err := fmt.Scan(&operand); err != nil {
-		fmt.Println(ErrorInvalidOperation)
-		return
-	}
+func main() {
+	var (
+		fOp, sOp int
+		operator string
+	)
 
-	var operation, ok = functionsMap[operand]
-	if !ok {
-		fmt.Println(ErrorInvalidOperation)
+	if _, err := fmt.Scan(&fOp, &sOp, &operator); err != nil {
+		fmt.Fprintf(os.Stderr, "There has been an error while reading input: %v\n", err)
 		return
 	}
 
-	if res, err := operation(fOp, sOp); err != nil {
-		fmt.Println(ErrorDivisionByZero)
+	if res, err := calculate(operator, fOp, sOp); err != nil {
+		fmt.Fprintf(os.Stderr, "There has been an error while calculating: %v\n", err)
 	} else {
 		fmt.Println(res)
 	}
