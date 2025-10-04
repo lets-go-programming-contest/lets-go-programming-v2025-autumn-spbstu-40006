c8d73ae5552fe26d2b3779410843e85a5af70193
Груздев <gruzdev_i06@mail.ru>
2025-10-04 22:41:56 +0300

    [TASK-2-2] added one type of recipient to the heap methods

--- ivan.gruzdev/task-2-2/cmd/service/main.go
+++ ivan.gruzdev/task-2-2/cmd/service/main.go
@@ -6,18 +6,18 @@ import (
 	"os"
 )
 
-type PriorityQueue []int //nolint:recvcheck
+type PriorityQueue []int
 
-func (pq PriorityQueue) Len() int {
-	return len(pq)
+func (pq *PriorityQueue) Len() int {
+	return len(*pq)
 }
 
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i] < pq[j]
+func (pq *PriorityQueue) Less(i, j int) bool {
+	return (*pq)[i] < (*pq)[j]
 }
 
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
+func (pq *PriorityQueue) Swap(i, j int) {
+	(*pq)[i], (*pq)[j] = (*pq)[j], (*pq)[i]
 }
 
 func (pq *PriorityQueue) Push(x interface{}) {
