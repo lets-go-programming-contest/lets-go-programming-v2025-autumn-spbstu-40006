7d316144ed5261af4936df089ddc499f8779b3de
DaniilNotResponding <gohankobol@gmail.com>
2025-10-04 18:39:29 +0300

    [TASK-1] Initial structure

---
+++ loboda.daniil/task-1/cmd/service/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	var op string
+
+	if _, err := fmt.Scan(&a); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	if _, err := fmt.Scan(&b); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	fmt.Scan(&op)
+
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(a / b)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
