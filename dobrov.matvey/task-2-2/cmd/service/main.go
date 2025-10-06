8c39000b2de646e58ff8d115448dff9a4c14ff01
HorekProgrammist <dobrovmatvey2018@gmail.com>
2025-10-06 11:51:18 +0300

    [TASK-2-2] Mini fixes for lint, I'll other fixes lately

--- dobrov.matvey/task-2-2/cmd/service/main.go
+++ dobrov.matvey/task-2-2/cmd/service/main.go
@@ -7,23 +7,30 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h *IntHeap) Push(x any) {
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+	*h = append(*h, v)
+}
+
 func (h *IntHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[:n-1]
+
 	return x
 }
 
 func main() {
-	var (
-		countDish, desiredDish int
-	)
+	var countDish, desiredDish int
 	_, err := fmt.Scan(&countDish)
+
 	if err != nil {
 		return
 	}
@@ -53,6 +60,7 @@ func main() {
 	}
 
 	_, err = fmt.Scan(&desiredDish)
+
 	if err != nil {
 		return
 	}
