aa02f3b725dd236d2c569e668f3f2c486acbcb11
HorekProgrammist <dobrovmatvey2018@gmail.com>
2025-10-06 11:39:57 +0300

    [TASK-2-2] Initital commit

---
+++ dobrov.matvey/task-2-2/cmd/service/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func main() {
+	var (
+		countDish, desiredDish int
+	)
+	_, err := fmt.Scan(&countDish)
+	if err != nil {
+		return
+	}
+
+	if countDish < 1 || countDish > 10000 {
+		return
+	}
+
+	dishRatings := make([]int, countDish)
+	for i := 0; i < countDish; i++ {
+		_, err = fmt.Scan(&dishRatings[i])
+
+		if err != nil {
+			return
+		}
+
+		if dishRatings[i] < -10000 || dishRatings[i] > 10000 {
+			return
+		}
+	}
+
+	dishRatingsHeap := &IntHeap{}
+	heap.Init(dishRatingsHeap)
+
+	for _, dishRating := range dishRatings {
+		heap.Push(dishRatingsHeap, dishRating)
+	}
+
+	_, err = fmt.Scan(&desiredDish)
+	if err != nil {
+		return
+	}
+
+	if desiredDish < 1 || desiredDish > countDish {
+		return
+	}
+
+	for i := 0; i < (desiredDish - 1); i++ {
+		heap.Pop(dishRatingsHeap)
+	}
+
+	fmt.Println(heap.Pop(dishRatingsHeap))
+}
