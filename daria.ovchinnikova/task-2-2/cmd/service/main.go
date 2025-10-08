91707a56551ec0e84f8b0de6dc4dfb51f9fb4407
Овчинникова Дарья <daraartovskaa@gmail.com>
2025-10-08 17:49:41 +0300

    [TASK-2-2] Fix naming, add blank lines, rewrite cycles

--- daria.ovchinnikova/task-2-2/cmd/service/main.go
+++ daria.ovchinnikova/task-2-2/cmd/service/main.go
@@ -7,43 +7,50 @@ import (
 
 type MaxHeap []int
 
-func (heap MaxHeap) Len() int {
-	return len(heap)
+func (h MaxHeap) Len() int {
+	return len(h)
 }
 
-func (heap MaxHeap) Less(i, j int) bool {
-	return heap[i] > heap[j]
+func (h MaxHeap) Less(i, j int) bool {
+	return h[i] > h[j]
 }
 
-func (heap MaxHeap) Swap(i, j int) {
-	heap[i], heap[j] = heap[j], heap[i]
+func (h MaxHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
 }
 
-func (heap *MaxHeap) Push(x interface{}) {
-	*heap = append(*heap, x.(int))
+func (h *MaxHeap) Push(x interface{}) {
+	value, ok := x.(int)
+	if !ok {
+		return
+	}
+	*h = append(*h, value)
 }
 
-func (heap *MaxHeap) Pop() interface{} {
-	old := *heap
+func (h *MaxHeap) Pop() interface{} {
+	old := *h
 	n := len(old)
 	x := old[n-1]
-	*heap = old[0 : n-1]
+	*h = old[0 : n-1]
+
 	return x
 }
 
 func main() {
-	var numberOfDishes, kNumber, choice int
+	var numberOfDishes, kNumber int
 	_, err := fmt.Scan(&numberOfDishes)
 	if err != nil {
 		fmt.Println("Invalid number of dishes")
+
 		return
 	}
 
 	ratings := make([]int, numberOfDishes)
-	for i := 0; i < numberOfDishes; i++ {
+	for i := range numberOfDishes {
 		_, err = fmt.Scan(&ratings[i])
 		if err != nil {
 			fmt.Println("Invalid rating")
+
 			return
 		}
 	}
@@ -51,6 +58,7 @@ func main() {
 	_, err = fmt.Scan(&kNumber)
 	if err != nil {
 		fmt.Println("Invalid k-number")
+
 		return
 	}
 
@@ -61,10 +69,11 @@ func main() {
 		heap.Push(ratingsHeap, rating)
 	}
 
-	for i := 0; i < kNumber; i++ {
-		choice = heap.Pop(ratingsHeap).(int)
+	for range kNumber - 1 {
+		heap.Pop(ratingsHeap)
 	}
 
+	choice := heap.Pop(ratingsHeap)
 	fmt.Println(choice)
 
 }
