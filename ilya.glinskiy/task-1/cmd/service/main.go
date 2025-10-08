fc166543bd04584c64cb4bf3b6d992d40eb99861
Глинский Илья <glinckyweb@gmail.com>
2025-10-08 16:18:31 +0300

    [TASK-1] made the calculator

--- ilya.glinskiy/task-1/cmd/service/main.go
+++ ilya.glinskiy/task-1/cmd/service/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var (
+		num1, num2 int
+		op         string
+	)
+
+	_, err := fmt.Scan(&num1)
+	if err != nil {
+		log.Fatal("Invalid first operand")
+	}
+
+	_, err = fmt.Scan(&num2)
+	if err != nil {
+		log.Fatal("Invalid second operand")
+	}
+
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		log.Fatal("Invalid operation")
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(num1 + num2)
+	case "-":
+		fmt.Println(num1 - num2)
+	case "*":
+		fmt.Println(num1 * num2)
+	case "/":
+		if num2 == 0 {
+			log.Fatal("Invalid operation")
+		}
+		fmt.Println(float64(num1) / float64(num2))
+	}
 }
