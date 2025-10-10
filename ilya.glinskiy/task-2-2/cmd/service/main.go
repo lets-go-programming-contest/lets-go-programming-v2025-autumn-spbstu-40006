28b80d67a37d01b75c6482abea8daf7e39d56f2c
Глинский Илья <glinckyweb@gmail.com>
2025-10-10 16:08:58 +0300

    [TASK-2-2] made IntHeap

--- ilya.glinskiy/task-2-2/cmd/service/main.go
+++ ilya.glinskiy/task-2-2/cmd/service/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"container/heap"
+	"fmt"
+)
 
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	var (
+		dishAmount, num int
+		err             error
+	)
+
+	dishHeap := &IntHeap{}
+	heap.Init(dishHeap)
+
+	_, err = fmt.Scan(&dishAmount)
+	if err != nil {
+		fmt.Println("Invalid dish amount")
+
+		return
+	}
+
+	for range dishAmount {
+		_, err = fmt.Scan(&num)
+		if err != nil {
+			fmt.Println("Invalid dish rating")
+
+			return
+		}
+
+		heap.Push(dishHeap, num)
+	}
+
+	_, err = fmt.Scan(&num)
+	if err != nil || num >= dishHeap.Len() {
+		fmt.Println("Invalid dish index")
+
+		return
+	}
+
+	fmt.Println((*dishHeap)[num])
 }
