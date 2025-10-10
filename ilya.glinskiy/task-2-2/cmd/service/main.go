38b0a4b6ae927595d22a07710213cd4c5cd6c7c3
Глинский Илья <glinckyweb@gmail.com>
2025-10-10 17:25:04 +0300

    [TASK-2-2] fixed formatting errors

--- ilya.glinskiy/task-2-2/cmd/service/main.go
+++ ilya.glinskiy/task-2-2/cmd/service/main.go
@@ -7,16 +7,16 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
+func (h *IntHeap) Len() int {
+	return len(*h)
 }
 
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *IntHeap) Push(x interface{}) {
@@ -28,6 +28,7 @@ func (h *IntHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
@@ -37,7 +38,8 @@ func main() {
 		err             error
 	)
 
-	var dishHeap *IntHeap = &IntHeap{}
+	dishHeap := &IntHeap{}
+
 	heap.Init(dishHeap)
 
 	_, err = fmt.Scan(&dishAmount)
