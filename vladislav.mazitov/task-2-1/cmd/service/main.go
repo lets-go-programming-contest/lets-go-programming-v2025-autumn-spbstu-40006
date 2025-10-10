60245884efb5e72aaa6fcf6ace7e5dc08b1210bb
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2025-10-10 16:19:59 +0300

    [TASK-2-1] linter fixes

--- vladislav.mazitov/task-2-1/cmd/service/main.go
+++ vladislav.mazitov/task-2-1/cmd/service/main.go
@@ -9,54 +9,87 @@ import (
 	"strings"
 )
 
-func main() {
-	reader := bufio.NewReader(os.Stdin)
+func istreamInt(reader *bufio.Reader) (int, error) {
 	input, err := reader.ReadString('\n')
 	if err != nil {
-		log.Fatal(err)
+
+		return 0, err
 	}
+
 	input = strings.TrimSpace(input)
-	n, err := strconv.Atoi(input)
+	val, err := strconv.Atoi(input)
+	if err != nil {
+
+		return 0, err
+	}
+
+	return val, nil
+}
+
+func istreamString(reader *bufio.Reader) (string, error) {
+	input, err := reader.ReadString('\n')
+	if err != nil {
+
+		return "", err
+	}
+
+	return strings.TrimSpace(input), nil
+}
+
+func checkTemperatures(check string, minValue *int, maxValue *int) int {
+	switch check[:2] {
+	case ">=":
+		temp, err := strconv.Atoi(strings.TrimSpace(check[3:]))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if temp > *minValue {
+			*minValue = temp
+		}
+	case "<=":
+		temp, err := strconv.Atoi(strings.TrimSpace(check[3:]))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if temp < *maxValue {
+			*maxValue = temp
+		}
+	}
+
+	if *minValue <= *maxValue {
+
+		return *minValue
+	} else {	
+
+		return -1
+	}
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	n, err := istreamInt(reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < n; i++ {
-		secondInput, err := reader.ReadString('\n')
+	for range n {
+		k, err := istreamInt(reader)
 		if err != nil {
 			log.Fatal(err)
 		}
-		secondInput = strings.TrimSpace(secondInput)
-		k, err := strconv.Atoi(secondInput)
-		if err != nil {
-			log.Fatal(err)
-		}
-		var min, max int = 15, 30
-		for j := 0; j < k; j++ {
-			thirdInput, err := reader.ReadString('\n')
+
+		var mnVal, mxVal = 15, 30
+
+		for range k {
+			str, err := istreamString(reader)
 			if err != nil {
 				log.Fatal(err)
 			}
-			check := (strings.TrimSpace(thirdInput))[:2]
-			value, err := strconv.Atoi(strings.TrimSpace(thirdInput)[3:])
-			if err != nil {
-				log.Fatal(err)
-			}
-			switch check {
-			case ">=":
-				if value > min {
-					min = value
-				}
-			case "<=":
-				if value < max {
-					max = value
-				}
-			}
-			if min <= max {
-				fmt.Println(min)
-			} else {
-				fmt.Println(-1)
-			}
+
+			fmt.Println(checkTemperatures(str, &mnVal, &mxVal))
 		}
 	}
 }
+
