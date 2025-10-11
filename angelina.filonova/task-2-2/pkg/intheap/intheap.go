7ce56164f7282f4ab8e8e085fb23c5126b271924
filonova.angelina <filonova2906@yandex.ru>
2025-10-11 20:54:13 +0300

    [TASK-2-2] fix for style

--- angelina.filonova/task-2-2/pkg/intheap/intheap.go
+++ angelina.filonova/task-2-2/pkg/intheap/intheap.go
@@ -2,17 +2,9 @@ package intheap
 
 type IntHeap []int
 
-func (h IntHeap) Len() int {
-	return len(h)
-}
-
-func (h IntHeap) Less(i, j int) bool {
-	return h[i] < h[j]
-}
-
-func (h IntHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
 	value, ok := x.(int)
@@ -24,10 +16,10 @@ func (h *IntHeap) Push(x any) {
 }
 
 func (h *IntHeap) Pop() any {
-	oldHeap := *h
-	n := len(oldHeap)
-	lastElement := oldHeap[n-1]
-	*h = oldHeap[0 : n-1]
+	old := *h
+	n := len(old)
+	last := old[n-1]
+	*h = old[:n-1]
 
-	return lastElement
+	return last
 }
