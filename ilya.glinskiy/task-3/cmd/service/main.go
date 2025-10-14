fb1d5e08208c774f5e550071df1f699eaeab9f7a
Глинский Илья <glinckyweb@gmail.com>
2025-10-14 14:20:40 +0300

    [TASK-3] made input for a config file

--- ilya.glinskiy/task-3/cmd/service/main.go
+++ ilya.glinskiy/task-3/cmd/service/main.go
@@ -1,5 +1,44 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+type Input struct {
+}
+
+func main() {
+	var config Config
+
+	if len(os.Args) < 3 || os.Args[1] != "-config" {
+		fmt.Println("Config file must be passed in parameters")
+
+		return
+	}
+
+	var configPath string = os.Args[2]
+
+	content, err := os.ReadFile(configPath)
+	if err != nil {
+		fmt.Println("Couldn't open config file")
+
+		return
+	}
+
+	err = yaml.Unmarshal(content, &config)
+	if err != nil {
+		fmt.Println("Something wrong I can feel it")
+
+		return
+	}
+
+	content, err = os.ReadFile(configPath)
 }
