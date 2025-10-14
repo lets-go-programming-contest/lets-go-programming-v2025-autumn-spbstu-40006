4175f5f0d3e2be9c52cbca596879ec67843b0892
Глинский Илья <glinckyweb@gmail.com>
2025-10-14 14:59:08 +0300

    [TASK-2-2] updated and moved IntHeap to intheap package

--- ilya.glinskiy/task-2-2/cmd/service/main.go
+++ ilya.glinskiy/task-2-2/cmd/service/main.go
@@ -3,38 +3,9 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/bloomkicks/task-2-2/internal/intheap"
 )
-
-type IntHeap []int
-
-func (h *IntHeap) Len() int {
-	return len(*h)
-}
-
-func (h *IntHeap) Less(i, j int) bool {
-	return (*h)[i] < (*h)[j]
-}
-
-func (h *IntHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *IntHeap) Push(x interface{}) {
-	i, ok := x.(int)
-
-	if ok {
-		*h = append(*h, i)
-	}
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
 
 func main() {
 	var (
@@ -42,7 +13,7 @@ func main() {
 		err             error
 	)
 
-	dishHeap := &IntHeap{}
+	dishHeap := &intheap.IntHeap{}
 
 	heap.Init(dishHeap)
 
@@ -65,7 +36,7 @@ func main() {
 	}
 
 	_, err = fmt.Scan(&num)
-	if err != nil || num > dishHeap.Len() {
+	if err != nil || num < 1 || num > dishHeap.Len() {
 		fmt.Println("Invalid dish index")
 
 		return
---
+++ ilya.glinskiy/task-2-2/internal/intheap/intheap.go
@@ -0,0 +1,33 @@
+package intheap
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int {
+	return len(*h)
+}
+
+func (h *IntHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
+}
+
+func (h *IntHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	i, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, i)
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
