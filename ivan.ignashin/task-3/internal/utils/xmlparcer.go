5278b40d9b20c90267ea603ee62c380e2a973aff
ivanignashin <ivanignashin@mail.ru>
2025-10-14 11:19:11 +0300

    [TASK-3] changed structures in XMLParcer

--- ivan.ignashin/task-3/internal/utils/xmlparcer.go
+++ ivan.ignashin/task-3/internal/utils/xmlparcer.go
@@ -5,24 +5,18 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
 	"golang.org/x/net/html/charset"
 )
 
 type Record struct {
-	ID    int     `xml:"id"`
-	Name  string  `xml:"name"`
-	Value float64 `xml:"value"`
+	ID    int     `xml:"numCode"`
+	Name  string  `xml:"CharCode"`
+	Value float64 `xml:"Value"`
 }
 
 type Records struct {
-	Items []struct {
-		ID    int    `xml:"NumCode"`
-		Name  string `xml:"CharCode"`
-		Value string `xml:"Value"`
-	} `xml:"Valute"`
+	Items []Record `xml:"Valute"`
 }
 
 func ParseXML(path string) ([]Record, error) {
@@ -43,15 +37,10 @@ func ParseXML(path string) ([]Record, error) {
 	records := make([]Record, 0, len(rawRecords.Items))
 
 	for _, item := range rawRecords.Items {
-		value, err := strconv.ParseFloat(strings.ReplaceAll(item.Value, ",", "."), 64)
-		if err != nil {
-			return nil, fmt.Errorf("parse float %s: %w", item.Value, err)
-		}
-
 		records = append(records, Record{
 			ID:    item.ID,
 			Name:  item.Name,
-			Value: value,
+			Value: item.Value,
 		})
 	}
 
