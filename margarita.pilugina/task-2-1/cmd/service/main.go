24a6c583f2fc7d7bbe45766455dc5313f6628643
Pilugina Margarita <margaritapiliugina2006@gmail.com>
2025-10-17 23:35:42 +0300

    [TASK-2-1] Initial commit

---
+++ margarita.pilugina/task-2-1/cmd/service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidNum       = errors.New("не является числом")
+	ErrInvalidParameter = errors.New("не является параметром")
+)
+
+func setTemperature(parameter string, temperature int, maxTemp, minTemp *int) (int, error) {
+	switch parameter {
+	case ">=":
+		if temperature > *minTemp {
+			*minTemp = temperature
+		}
+	case "<=":
+		if temperature < *maxTemp {
+			*maxTemp = temperature
+		}
+	default:
+		return -1, ErrInvalidParameter
+	}
+
+	if *minTemp > *maxTemp {
+		return -1, nil
+	}
+	return *minTemp, nil
+}
+
+func main() {
+	var (
+		numDep, dearColleagues, temperature int
+		parameter                           string
+	)
+
+	if _, err := fmt.Scan(&numDep); err != nil {
+		fmt.Println(ErrInvalidNum)
+		return
+	}
+
+	for depI := 0; depI < numDep; depI++ {
+		maxTemp, minTemp:= 30, 15
+		if _, err := fmt.Scan(&dearColleagues); err != nil {
+		fmt.Println(ErrInvalidNum)
+		return
+		}
+		for dcI := 0; dcI < dearColleagues; dcI++ {
+			if _, err := fmt.Scan(&parameter, &temperature); err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			out, err := setTemperature(parameter, temperature, &maxTemp, &minTemp)
+			if err != nil {
+				fmt.Println(out)
+				return
+			}
+			fmt.Println(out)
+		}
+	}
+}
