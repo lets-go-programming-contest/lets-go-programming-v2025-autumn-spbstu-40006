047284030faccfb8a771a0f92d954091e95a7613
Дарья <shyriaevaea@gmail.com>
2025-10-17 23:25:40 +0300

    [TASK-2-2] fixed lint2

--- shiryaeva.daria/task-2-2/cmd/service/main.go
+++ shiryaeva.daria/task-2-2/cmd/service/main.go
@@ -7,12 +7,16 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Len() int { return len(h) }
+
 func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
 
 func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+	if value, ok := x.(int); ok {
+		*h = append(*h, value)
+	}
 }
 
 func (h *IntHeap) Pop() interface{} {
@@ -20,45 +24,44 @@ func (h *IntHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
-
 	return x
 }
 
 func main() {
-	var n, k int
+	var numberOfElements, kElement int
 
-	_, err := fmt.Scan(&n)
+	_, err := fmt.Scan(&numberOfElements)
 	if err != nil {
-
 		return
 	}
 
-	ratings := make([]int, n)
-	for i := 0; i < n; i++ {
-		_, err = fmt.Scan(&ratings[i])
+	ratings := make([]int, numberOfElements)
+	for index := range numberOfElements {
+		_, err = fmt.Scan(&ratings[index])
 		if err != nil {
-
 			return
 		}
 	}
 
-	_, err = fmt.Scan(&k)
+	_, err = fmt.Scan(&kElement)
 	if err != nil {
-
 		return
 	}
 
-	h := &IntHeap{}
-	heap.Init(h)
+	ratingsHeap := &IntHeap{}
+	heap.Init(ratingsHeap)
 
 	for _, rating := range ratings {
-		heap.Push(h, rating)
+		heap.Push(ratingsHeap, rating)
 	}
 
-	for i := 0; i < k-1; i++ {
-		heap.Pop(h)
+	for range kElement - 1 {
+		heap.Pop(ratingsHeap)
 	}
 
-	result := heap.Pop(h).(int)
+	result, ok := heap.Pop(ratingsHeap).(int)
+	if !ok {
+		return
+	}
 	fmt.Println(result)
 }
