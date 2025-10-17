da2cadb9f98006eb759232cc83250dc81b95395a
Овчинникова Дарья <daraartovskaa@gmail.com>
2025-10-17 19:55:05 +0300

    [TASK-2-2] Divide into packages and add error output

--- daria.ovchinnikova/task-2-2/cmd/service/main.go
+++ daria.ovchinnikova/task-2-2/cmd/service/main.go
@@ -3,39 +3,9 @@ package main
 import (
 	"container/heap"
 	"fmt"
+
+	"github.com/monka6/task-2-2/intheap"
 )
-
-type MaxHeap []int
-
-func (h *MaxHeap) Len() int {
-	return len(*h)
-}
-
-func (h *MaxHeap) Less(i, j int) bool {
-	return (*h)[i] > (*h)[j]
-}
-
-func (h *MaxHeap) Swap(i, j int) {
-	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
-}
-
-func (h *MaxHeap) Push(x interface{}) {
-	value, ok := x.(int)
-	if !ok {
-		return
-	}
-
-	*h = append(*h, value)
-}
-
-func (h *MaxHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-
-	return x
-}
 
 func main() {
 	var numberOfDishes, kNumber int
@@ -65,7 +35,7 @@ func main() {
 		return
 	}
 
-	ratingsHeap := &MaxHeap{}
+	ratingsHeap := &intheap.MaxHeap{}
 	heap.Init(ratingsHeap)
 
 	for _, rating := range ratings {
