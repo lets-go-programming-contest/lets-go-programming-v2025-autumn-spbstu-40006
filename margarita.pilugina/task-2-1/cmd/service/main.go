c7cdd7cc12cf2a1a931e63bdfaae1ba474241f12
Pilugina Margarita <margaritapiliugina2006@gmail.com>
2025-10-18 01:06:06 +0300

    [TASK-2-1] lint fixes

--- margarita.pilugina/task-2-1/cmd/service/main.go
+++ margarita.pilugina/task-2-1/cmd/service/main.go
@@ -21,12 +21,16 @@ func setTemperature(parameter string, temperature int, maxTemp, minTemp *int) (i
 			*maxTemp = temperature
 		}
 	default:
+
+
 		return -1, ErrInvalidParameter
 	}
 
 	if *minTemp > *maxTemp {
-		return -1, nil
+
+		return -1, ErrInvalidParameter
 	}
+
 	return *minTemp, nil
 }
 
@@ -38,26 +42,37 @@ func main() {
 
 	if _, err := fmt.Scan(&numDep); err != nil {
 		fmt.Println(ErrInvalidNum)
+
 		return
 	}
 
-	for depI := 0; depI < numDep; depI++ {
-		maxTemp, minTemp:= 30, 15
+	for depI := range numDep {
+		_ = depI
+
+		maxTemp, minTemp := 30, 15
+
 		if _, err := fmt.Scan(&dearColleagues); err != nil {
-		fmt.Println(ErrInvalidNum)
-		return
+			fmt.Println(ErrInvalidNum)
+
+			return
 		}
-		for dcI := 0; dcI < dearColleagues; dcI++ {
+
+		for dcI := range dearColleagues {
+			_ = dcI
+
 			if _, err := fmt.Scan(&parameter, &temperature); err != nil {
-				fmt.Println(err)
+				fmt.Println(ErrInvalidNum)
+
 				return
 			}
 
 			out, err := setTemperature(parameter, temperature, &maxTemp, &minTemp)
 			if err != nil {
-				fmt.Println(out)
+				fmt.Println(err)
+
 				return
 			}
+
 			fmt.Println(out)
 		}
 	}
