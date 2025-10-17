c5612aac022809e87f4922957475c9734f01731b
Дарья <shyriaevaea@gmail.com>
2025-10-17 23:29:27 +0300

    [TASK-2-2] fixed lint3

--- shiryaeva.daria/task-2-2/cmd/service/main.go
+++ shiryaeva.daria/task-2-2/cmd/service/main.go
@@ -7,11 +7,11 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int { return len(h) }
+func (h *IntHeap) Len() int { return len(*h) }
 
-func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
 
-func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x interface{}) {
 	if value, ok := x.(int); ok {
@@ -24,6 +24,7 @@ func (h *IntHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
@@ -63,5 +64,6 @@ func main() {
 	if !ok {
 		return
 	}
+
 	fmt.Println(result)
 }
