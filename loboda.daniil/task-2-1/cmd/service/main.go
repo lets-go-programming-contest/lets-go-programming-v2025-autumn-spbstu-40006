5c3fd95ca9a55b2bf2bf04870ec2f72aed2c46a9
DaniilNotResponding <gohankobol@gmail.com>
2025-10-18 18:28:06 +0300

    [TASK-2-1] fix  input

--- loboda.daniil/task-2-1/cmd/service/main.go
+++ loboda.daniil/task-2-1/cmd/service/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strconv"
+	"os"
 )
 
 const (
@@ -10,89 +11,47 @@ const (
 	baseUpper = 30
 )
 
-func readConstraint() (string, int, bool) {
-	var operatorToken string
-	var temperature int
-
-	fieldsRead, err := fmt.Scan(&operatorToken, &temperature)
-	if err != nil || fieldsRead != 2 {
-		return "", 0, false
-	}
-	if operatorToken != ">=" && operatorToken != "<=" {
-		return "", 0, false
-	}
-	return operatorToken, temperature, true
-}
-
-func readCounts() (int, int, bool, string, int, bool) {
-	var firstNumber int
-	if read, err := fmt.Scan(&firstNumber); err != nil || read != 1 || firstNumber < 0 {
-		return 0, 0, false, "", 0, false
-	}
-
-	var secondToken string
-	readSecond, _ := fmt.Scan(&secondToken)
-	if readSecond != 1 {
-		return 1, firstNumber, false, "", 0, true
-	}
-	if numeric, err := strconv.Atoi(secondToken); err == nil {
-		return firstNumber, numeric, false, "", 0, true
-	}
-
-	var temperature int
-	if read, err := fmt.Scan(&temperature); err != nil || read != 1 {
-		return 0, 0, false, "", 0, false
-	}
-	return 1, firstNumber, true, secondToken, temperature, true
-}
-
-func applyConstraint(lower, upper int, operatorToken string, temperature int) (int, int) {
-	switch operatorToken {
-	case ">=":
-		if temperature > lower {
-			lower = temperature
-		}
-	case "<=":
-		if temperature < upper {
-			upper = temperature
-		}
-	}
-	return lower, upper
-}
-
-func printState(lower, upper int) {
-	if lower <= upper {
-		fmt.Println(lower)
-
-		return
-	}
-	fmt.Println(-1)
-}
-
 func main() {
-	departments, limitsPerDepartment, haveFirst, firstOperator, firstTemperature, ok := readCounts()
-	if !ok {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var departments, employees int
+
+	if _, err := fmt.Fscan(in, &departments, &employees); err != nil {
+
 		return
 	}
 
 	for i := 0; i < departments; i++ {
-		lowerBound, upperBound := baseLower, baseUpper
+		lower, upper := baseLower, baseUpper
+		for j := 0; j < employees; j++ {
+			var op string
+			var t int
 
-		for j := 0; j < limitsPerDepartment; j++ {
-			if haveFirst && j == 0 {
-				lowerBound, upperBound = applyConstraint(lowerBound, upperBound, firstOperator, firstTemperature)
-				printState(lowerBound, upperBound)
-				continue
+			if _, err := fmt.Fscan(in, &op, &t); err != nil {
+
+				return
 			}
+			switch op {
+			case ">=":
+				if t > lower {
+					lower = t
+				}
+			case "<=":
+				if t < upper {
+					upper = t
+				}
+			default:
 
-			operatorToken, temperature, valid := readConstraint()
-			if !valid {
 				return
 			}
 
-			lowerBound, upperBound = applyConstraint(lowerBound, upperBound, operatorToken, temperature)
-			printState(lowerBound, upperBound)
+			if lower <= upper {
+				fmt.Fprintln(out, lower)
+			} else {
+				fmt.Fprintln(out, -1)
+			}
 		}
-		haveFirst = false
 	}
 }
