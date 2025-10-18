4df4ba3f6cf195c112205549c28d186c287e335f
DaniilNotResponding <gohankobol@gmail.com>
2025-10-18 17:49:43 +0300

    [TASK-2-1] fix  tests

--- loboda.daniil/task-2-1/cmd/service/main.go
+++ loboda.daniil/task-2-1/cmd/service/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 const (
 	baseLower = 15
@@ -17,12 +20,32 @@ func readConstraint() (string, int, bool) {
 	if err != nil || fieldsRead != 2 {
 		return "", 0, false
 	}
-
 	if operatorToken != ">=" && operatorToken != "<=" {
 		return "", 0, false
 	}
-
 	return operatorToken, temperature, true
+}
+
+func readCounts() (int, int, bool, string, int, bool) {
+	var firstNumber int
+	if read, err := fmt.Scan(&firstNumber); err != nil || read != 1 || firstNumber < 0 {
+		return 0, 0, false, "", 0, false
+	}
+
+	var secondToken string
+	readSecond, _ := fmt.Scan(&secondToken)
+	if readSecond != 1 {
+		return 1, firstNumber, false, "", 0, true
+	}
+	if numeric, err := strconv.Atoi(secondToken); err == nil {
+		return firstNumber, numeric, false, "", 0, true
+	}
+
+	var temperature int
+	if read, err := fmt.Scan(&temperature); err != nil || read != 1 {
+		return 0, 0, false, "", 0, false
+	}
+	return 1, firstNumber, true, secondToken, temperature, true
 }
 
 func applyConstraint(lower, upper int, operatorToken string, temperature int) (int, int) {
@@ -36,7 +59,6 @@ func applyConstraint(lower, upper int, operatorToken string, temperature int) (i
 			upper = temperature
 		}
 	}
-
 	return lower, upper
 }
 
@@ -46,26 +68,33 @@ func printState(lower, upper int) {
 
 		return
 	}
-
 	fmt.Println(-1)
 }
 
 func main() {
-	var limitsCount int
-	if read, err := fmt.Scan(&limitsCount); err != nil || read != 1 || limitsCount < 0 {
+	departments, limitsPerDepartment, haveFirst, firstOperator, firstTemperature, ok := readCounts()
+	if !ok {
 		return
 	}
 
-	lowerBound, upperBound := baseLower, baseUpper
+	for range departments {
+		lowerBound, upperBound := baseLower, baseUpper
 
-	for range limitsCount {
-		operatorToken, temperature, valid := readConstraint()
-		if !valid {
-			return
+		for idx := range limitsPerDepartment {
+			if haveFirst && idx == 0 {
+				lowerBound, upperBound = applyConstraint(lowerBound, upperBound, firstOperator, firstTemperature)
+				printState(lowerBound, upperBound)
+
+				continue
+			}
+
+			operatorToken, temperature, valid := readConstraint()
+			if !valid {
+				return
+			}
+			lowerBound, upperBound = applyConstraint(lowerBound, upperBound, operatorToken, temperature)
+			printState(lowerBound, upperBound)
 		}
-
-		lowerBound, upperBound = applyConstraint(lowerBound, upperBound, operatorToken, temperature)
-
-		printState(lowerBound, upperBound)
+		haveFirst = false
 	}
 }
