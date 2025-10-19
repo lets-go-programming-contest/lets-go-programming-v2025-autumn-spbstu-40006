154fc74f75df4ba916277032d6611cf2a3561447
DaniilNotResponding <gohankobol@gmail.com>
2025-10-19 16:53:13 +0300

    [TASK-2-2] fix lint

--- loboda.daniil/task-2-2/cmd/service/main.go
+++ loboda.daniil/task-2-2/cmd/service/main.go
@@ -7,47 +7,46 @@ import (
 
 type IntMinHeap []int
 
-func (h IntMinHeap) Len() int {
-	return len(h)
-}
-func (h IntMinHeap) Less(i, j int) bool {
-	return h[i] < h[j]
-}
-func (h IntMinHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
-}
+func (h *IntMinHeap) Len() int { return len(*h) }
+
+func (h *IntMinHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+
+func (h *IntMinHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
 func (h *IntMinHeap) Push(x any) {
-	*h = append(*h, x.(int))
+	v, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, v)
 }
+
 func (h *IntMinHeap) Pop() any {
 	old := *h
 	n := len(old)
 	x := old[n-1]
+
 	*h = old[:n-1]
+
 	return x
 }
 
 func main() {
-
-	var number, kPosition int
-
-	if _, err := fmt.Scan(&number); err != nil || (number < 1 && number > 10000) {
-		fmt.Println("Invalid number dishes")
+	var dishCount, kPosition int
+	if _, err := fmt.Scan(&dishCount); err != nil || dishCount < 1 || dishCount > 10000 {
 		return
 	}
-	ratings := make([]int, number)
-	maxDish := 0
-	for i := 0; i < number; i++ {
+
+	ratings := make([]int, dishCount)
+
+	for i := range dishCount {
 		if _, err := fmt.Scan(&ratings[i]); err != nil {
-			fmt.Println("Invalid number dishes")
 			return
 		}
-		if ratings[i] > maxDish {
-			maxDish = ratings[i]
-		}
 	}
-	if _, err := fmt.Scan(&kPosition); err != nil || (kPosition < -10000 && kPosition > 10000) {
-		fmt.Println("Invalid K number dish")
+
+	if _, err := fmt.Scan(&kPosition); err != nil || kPosition < 1 || kPosition > dishCount {
 		return
 	}
 
@@ -56,11 +55,11 @@ func main() {
 
 	for _, rating := range ratings {
 		heap.Push(minHeap, rating)
+
 		if minHeap.Len() > kPosition {
 			heap.Pop(minHeap)
 		}
 	}
 
 	fmt.Println((*minHeap)[0])
-
 }
