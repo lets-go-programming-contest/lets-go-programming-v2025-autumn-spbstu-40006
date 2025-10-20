da3232dfbfd634d63f0c8b1eb200abe6515ee623
filonova.angelina <filonova2906@yandex.ru>
2025-10-20 23:38:03 +0300

    [TASK-3] fix lint 5

--- angelina.filonova/task-3/internal/parser/parser.go
+++ angelina.filonova/task-3/internal/parser/parser.go
@@ -21,9 +21,9 @@ type ValCurs struct {
 }
 
 type Valute struct {
-	NumCode  int     `json:"num_code" xml:"NumCode"`
+	NumCode  int     `json:"num_code"  xml:"NumCode"`
 	CharCode string  `json:"char_code" xml:"CharCode"`
-	Value    float64 `json:"value" xml:"Value"`
+	Value    float64 `json:"value"     xml:"Value"`
 }
 
 func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) error {
@@ -43,6 +43,7 @@ func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) erro
 
 	str := strings.ReplaceAll(aux.Value, ",", ".")
 	val, err := strconv.ParseFloat(str, 64)
+
 	if err != nil {
 		return fmt.Errorf("invalid value %q: %w", aux.Value, err)
 	}
