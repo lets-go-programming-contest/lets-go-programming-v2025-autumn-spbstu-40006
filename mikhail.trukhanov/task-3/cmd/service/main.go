85d72cb0c48a004bb7eb0ec371e3301c48f9bf55
Mishaa105 <truhanov2006@mail.ru>
2025-10-22 20:33:21 +0300

    [TASK-3] fourth path fix

--- mikhail.trukhanov/task-3/cmd/service/main.go
+++ mikhail.trukhanov/task-3/cmd/service/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/Mishaa105/task-3/internal/decoding"
 	"github.com/Mishaa105/task-3/internal/saving"
@@ -10,11 +12,17 @@ import (
 
 func main() {
 	configPath := flag.String("config", "config.yaml", "path to config")
-	outputPath := flag.String("output", ".output/result.json", "path to output file")
+	outputPath := flag.String("output", "output/result.json", "path to output file")
 	flag.Parse()
 
+	absOutputPath, err := filepath.Abs(*outputPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error resolving absolute output path: %v\n", err)
+		os.Exit(1)
+	}
+
 	valCurs := decoding.Decoding(*configPath)
-	saving.SaveToJSON(*outputPath, valCurs.Valutes)
+	saving.SaveToJSON(absOutputPath, valCurs.Valutes)
 
 	for _, val := range valCurs.Valutes {
 		fmt.Printf("NumCode: %d, CharCode: %s, Value: %.2f\n", val.NumCode, val.CharCode, val.Value)
