dd6c5452bd54cb64d352fca8c9f8ac892056fd76
Mishaa105 <truhanov2006@mail.ru>
2025-10-22 19:32:35 +0300

    [TASK-3] second path fix

--- mikhail.trukhanov/task-3/cmd/service/main.go
+++ mikhail.trukhanov/task-3/cmd/service/main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	configPath := flag.String("config", "config.yaml", "path")
+	configPath := flag.String("config", "config.yaml", "path to config")
+	outputPath := flag.String("output", "output/result.json", "path to output file")
 	flag.Parse()
 
 	valCurs := decoding.Decoding(*configPath)
+	saving.SaveToJSON(*outputPath, valCurs.Valutes)
 
 	for _, val := range valCurs.Valutes {
 		fmt.Printf("NumCode: %d, CharCode: %s, Value: %.2f\n", val.NumCode, val.CharCode, val.Value)
 	}
-
-	saving.SaveToJSON("output/result.json", valCurs.Valutes)
 }
