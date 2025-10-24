68756f9f8a69990e4ae5d8b2e5729d39870942f5
Груздев <gruzdev_i06@mail.ru>
2025-10-25 02:12:34 +0300

    [TASK-3] Fix linter part 7

--- ivan.gruzdev/task-3/pkg/currency/types.go
+++ ivan.gruzdev/task-3/pkg/currency/types.go
@@ -14,9 +14,9 @@ type ValCurs struct {
 }
 
 type Currency struct {
-	NumCode  int           `xml:"NumCode"  json:"num_code"`
-	CharCode string        `xml:"CharCode" json:"char_code"`
-	Value    CurrencyValue `xml:"Value"    json:"value"`
+	NumCode  int           `json:"num_code" xml:"NumCode"`
+	CharCode string        `json:"char_code" xml:"CharCode"`
+	Value    CurrencyValue `json:"value" xml:"Value"`
 }
 
 func (currencyValue *CurrencyValue) UnmarshalXML(decoder *xml.Decoder, startElement xml.StartElement) error {
