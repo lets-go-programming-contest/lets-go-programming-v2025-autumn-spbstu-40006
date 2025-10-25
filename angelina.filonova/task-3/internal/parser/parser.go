11fbc7f0358508829b8141d241e8481f4e324894
filonova.angelina <filonova2906@yandex.ru>
2025-10-25 15:28:36 +0300

    [TASK-3] fix formatted (gofumpt)

--- angelina.filonova/task-3/internal/parser/parser.go
+++ angelina.filonova/task-3/internal/parser/parser.go
@@ -43,7 +43,6 @@ func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) erro
 
 	str := strings.ReplaceAll(aux.Value, ",", ".")
 	val, err := strconv.ParseFloat(str, 64)
-
 	if err != nil {
 		return fmt.Errorf("invalid value %q: %w", aux.Value, err)
 	}
