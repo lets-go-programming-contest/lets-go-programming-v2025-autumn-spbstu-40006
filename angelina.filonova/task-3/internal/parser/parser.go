7d00b2ba744d131d334a880778371ef0d4854a7c
filonova.angelina <filonova2906@yandex.ru>
2025-10-25 15:38:22 +0300

    [TASK-3] fix lint :)

--- angelina.filonova/task-3/internal/parser/parser.go
+++ angelina.filonova/task-3/internal/parser/parser.go
@@ -42,13 +42,11 @@ func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) erro
 	}
 
 	str := strings.ReplaceAll(aux.Value, ",", ".")
-	val, err := strconv.ParseFloat(str, 64)
-
+	var err error
+	v.Value, err = strconv.ParseFloat(str, 64)
 	if err != nil {
 		return fmt.Errorf("invalid value %q: %w", aux.Value, err)
 	}
-
-	v.Value = val
 
 	return nil
 }
