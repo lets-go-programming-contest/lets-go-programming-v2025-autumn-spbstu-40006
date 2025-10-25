0ca4bc4108c630bf9e2dae09d885f21f2b28649b
filonova.angelina <filonova2906@yandex.ru>
2025-10-25 15:32:40 +0300

    [TASK-3] fix wsl

--- angelina.filonova/task-3/internal/parser/parser.go
+++ angelina.filonova/task-3/internal/parser/parser.go
@@ -43,6 +43,7 @@ func (v *Valute) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) erro
 
 	str := strings.ReplaceAll(aux.Value, ",", ".")
 	val, err := strconv.ParseFloat(str, 64)
+
 	if err != nil {
 		return fmt.Errorf("invalid value %q: %w", aux.Value, err)
 	}
