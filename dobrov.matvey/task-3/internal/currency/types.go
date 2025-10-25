61ac4c06159994d659d694bada5c80d0305e8643
HorekProgrammist <dobrovmatvey2018@gmail.com>
2025-10-25 16:05:05 +0300

    [TASK-3] Added UnmarshalXML

--- dobrov.matvey/task-3/internal/currency/types.go
+++ dobrov.matvey/task-3/internal/currency/types.go
@@ -1,6 +1,33 @@
 package currency
 
+import (
+	"encoding/xml"
+	"strconv"
+	"strings"
+)
+
 type CurrencyValue float64
+
+func (cv *CurrencyValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var s string
+
+	err := d.DecodeElement(&s, &start)
+	if err != nil {
+		return err
+	}
+
+	s = strings.TrimSpace(s)
+	s = strings.ReplaceAll(s, "\u00A0", "")
+	s = strings.ReplaceAll(s, ",", ".")
+
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return err
+	}
+
+	*cv = CurrencyValue(f)
+	return nil
+}
 
 type ValCurs struct {
 	Currencies []Currency `xml:"Valute"`
