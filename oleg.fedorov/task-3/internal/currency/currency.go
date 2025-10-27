59b39cf9ec15d5b47df9c5e2e4f89b6dc97509f4
Fedorov <fedorovo727@gmail.com>
2025-10-27 14:45:39 +0300

    [TASK-3] fixing linter tag

--- oleg.fedorov/task-3/internal/currency/currency.go
+++ oleg.fedorov/task-3/internal/currency/currency.go
@@ -8,37 +8,35 @@ import (
 )
 
 type Currency struct {
-	XMLName  xml.Name `xml:"Valute" json:"-"`
+	XMLName  xml.Name `xml:"Valute"  json:"-"`
 	ID       string   `xml:"ID,attr" json:"-"`
 	NumCode  int      `xml:"NumCode" json:"num_code"`
 	CharCode string   `xml:"CharCode" json:"char_code"`
 	Nominal  int      `xml:"Nominal" json:"-"`
-	Name     string   `xml:"Name" json:"-"`
-	Value    Float64  `xml:"Value" json:"value"`
+	Name     string   `xml:"Name"     json:"-"`
+	Value    Float64  `xml:"Value"    json:"value"`
 }
 
 type Float64 float64
 
-type Currencies struct {
-	XMLName    xml.Name   `xml:"ValCurs"`
-	Currencies []Currency `xml:"Valute"`
-}
-
 func (f *Float64) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	var s string
-
-	if err := d.DecodeElement(&s, &start); err != nil {
-		return err
+	var valueString string
+	if err := d.DecodeElement(&valueString, &start); err != nil {
+		return fmt.Errorf("failed to decode element: %w", err)
 	}
 
-	s = strings.Replace(s, ",", ".", -1)
+	valueString = strings.ReplaceAll(valueString, ",", ".")
 
-	val, err := strconv.ParseFloat(s, 64)
+	val, err := strconv.ParseFloat(valueString, 64)
 	if err != nil {
-		return fmt.Errorf("failed to parse currency value '%s': %w", s, err)
+		return fmt.Errorf("failed to parse currency value '%s': %w", valueString, err)
 	}
 
 	*f = Float64(val)
-
 	return nil
+}
+
+type Currencies struct {
+	XMLName    xml.Name   `xml:"ValCurs"`
+	Currencies []Currency `xml:"Valute"`
 }
