cd55830d03f4909f33c090830cf0315926df6620
Дарья <shyriaevaea@gmail.com>
2025-10-28 23:16:41 +0300

    [TASK-3] fix lint

--- shiryaeva.daria/task-3/internal/config/config.go
+++ shiryaeva.daria/task-3/internal/config/config.go
@@ -1,27 +1,27 @@
 package config
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
-    InputFile  string `yaml:"input-file"`
-    OutputFile string `yaml:"output-file"`
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
 }
 
 func Load(path string) (*Config, error) {
-    data, err := os.ReadFile(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read config file '%s': %w", path, err)
-    }
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file '%s': %w", path, err)
+	}
 
-    var cfg Config
-    if err := yaml.Unmarshal(data, &cfg); err != nil {
-        return nil, fmt.Errorf("failed to parse YAML config: %w", err)
-    }
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse YAML config: %w", err)
+	}
 
-    return &cfg, nil
+	return &cfg, nil
 }
