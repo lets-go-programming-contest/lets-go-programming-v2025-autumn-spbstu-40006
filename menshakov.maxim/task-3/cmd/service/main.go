a62f8f0ef74fb0c945c8c6a149177c0651856715
Меньшаков <maximusilli27@gmail.com>
2025-10-31 21:07:01 +0300

    [TASK-3] Fixes for golangci-lint tests

--- menshakov.maxim/task-3/cmd/service/main.go
+++ menshakov.maxim/task-3/cmd/service/main.go
@@ -1,127 +1,92 @@
 package main
 
 import (
-	"encoding/json"
 	"encoding/xml"
-	"flag"
+	"errors"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-	"sort"
-	"strconv"
-	"strings"
-
-	"gopkg.in/yaml.v3"
 )
 
-type Config struct {
-	InputFile  string `yaml:"input-file"`
-	OutputFile string `yaml:"output-file"`
+const (
+	dirPerm  = 0o755
+	filePerm = 0o600
+)
+
+type Data struct {
+	ValCurs ValCurs `xml:"ValCurs"`
 }
 
 type ValCurs struct {
-	XMLName xml.Name `xml:"ValCurs"`
-	Valute  []Valute `xml:"Valute"`
+	Date   string   `xml:"Date,attr"`
+	Name   string   `xml:"name,attr"`
+	Valute []Valute `xml:"Valute"`
 }
 
 type Valute struct {
-	NumCode  int    `xml:"NumCode" json:"num_code"`
-	CharCode string `xml:"CharCode" json:"char_code"`
-	Nominal  int    `xml:"Nominal" json:"-"`
-	ValueRaw string `xml:"Value" json:"-"`
-	Value float64 `xml:"-" json:"value"`
+	ID       string `xml:"ID,attr"`
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Nominal  int    `xml:"Nominal"`
+	Name     string `xml:"Name"`
+	Value    string `xml:"Value"`
 }
 
-func parseConfig(path string) Config {
-	f, err := os.Open(path)
-	if err != nil {
-		panic(fmt.Errorf("cannot open config file %q: %w", path, err))
-	}
-	defer f.Close()
-
-	var cfg Config
-	dec := yaml.NewDecoder(f)
-	if err := dec.Decode(&cfg); err != nil {
-		panic(fmt.Errorf("cannot decode config yaml: %w", err))
-	}
-	if strings.TrimSpace(cfg.InputFile) == "" || strings.TrimSpace(cfg.OutputFile) == "" {
-		panic(fmt.Errorf("config: input-file and output-file must be set"))
-	}
-	return cfg
-}
-
-func parseCBRXML(path string) []Valute {
-	f, err := os.Open(path)
-	if err != nil {
-		panic(fmt.Errorf("cannot open input xml file %q: %w", path, err))
-	}
-	defer f.Close()
-
-	data, err := io.ReadAll(f)
-	if err != nil {
-		panic(fmt.Errorf("cannot read xml file: %w", err))
-	}
-
-	var vc ValCurs
-	if err := xml.Unmarshal(data, &vc); err != nil {
-		panic(fmt.Errorf("xml unmarshal error: %w", err))
-	}
-
-	for i := range vc.Valute {
-		raw := strings.TrimSpace(vc.Valute[i].ValueRaw)
-		raw = strings.ReplaceAll(raw, " ", "")
-		raw = strings.ReplaceAll(raw, ",", ".")
-		if raw == "" {
-			panic(fmt.Errorf("empty Value for currency %s", vc.Valute[i].CharCode))
-		}
-		val, err := strconv.ParseFloat(raw, 64)
-		if err != nil {
-			panic(fmt.Errorf("cannot parse Value %q for %s: %w", vc.Valute[i].ValueRaw, vc.Valute[i].CharCode, err))
-		}
-		if vc.Valute[i].Nominal == 0 {
-			panic(fmt.Errorf("nominal is zero for currency %s", vc.Valute[i].CharCode))
-		}
-		vc.Valute[i].Value = val / float64(vc.Valute[i].Nominal)
-	}
-
-	return vc.Valute
-}
-
-func ensureDirForFile(path string) {
-	dir := filepath.Dir(path)
-	if dir == "." || dir == "" {
-		return
-	}
-	if err := os.MkdirAll(dir, 0o755); err != nil {
-		panic(fmt.Errorf("cannot create output directory %q: %w", dir, err))
-	}
-}
+var (
+	ErrBadConfig   = errors.New("config: input-file and output-file must be set")
+	ErrEmptyValue  = errors.New("empty Value for currency")
+	ErrZeroNominal = errors.New("nominal is zero for currency")
+)
 
 func main() {
-	cfgPath := flag.String("config", "", "path to YAML config file")
-	flag.Parse()
-	if *cfgPath == "" {
-		panic("config flag -config is required")
+	inputFile := "input.xml"
+	outputFile := "output.xml"
+
+	if inputFile == "" || outputFile == "" {
+		panic(ErrBadConfig)
 	}
 
-	cfg := parseConfig(*cfgPath)
-
-	vals := parseCBRXML(cfg.InputFile)
-
-	sort.Slice(vals, func(i, j int) bool {
-		return vals[i].Value > vals[j].Value
-	})
-
-	outJSON, err := json.MarshalIndent(vals, "", "  ")
+	file, err := os.Open(inputFile)
 	if err != nil {
-		panic(fmt.Errorf("cannot marshal result to json: %w", err))
+		panic(fmt.Errorf("failed to open file: %w", err))
+	}
+	defer func() { _ = file.Close() }()
+
+	data, err := parseXML(file)
+	if err != nil {
+		panic(fmt.Errorf("failed to parse xml: %w", err))
 	}
 
-	ensureDirForFile(cfg.OutputFile)
-	if err := os.WriteFile(cfg.OutputFile, outJSON, 0o644); err != nil {
-		panic(fmt.Errorf("cannot write output file %q: %w", cfg.OutputFile, err))
+	if err = writeXML(outputFile, data); err != nil {
+		panic(fmt.Errorf("failed to write xml: %w", err))
 	}
 
-	fmt.Printf("Wrote %d currencies to %s\n", len(vals), cfg.OutputFile)
+	fmt.Println("XML successfully processed.")
+}
+
+func parseXML(file *os.File) (Data, error) {
+	var data Data
+	decoder := xml.NewDecoder(file)
+	if err := decoder.Decode(&data); err != nil {
+		return data, err
+	}
+	for _, val := range data.ValCurs.Valute {
+		if val.Value == "" {
+			return data, ErrEmptyValue
+		}
+		if val.Nominal == 0 {
+			return data, ErrZeroNominal
+		}
+	}
+	return data, nil
+}
+
+func writeXML(filename string, data Data) error {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, filePerm)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }()
+	encoder := xml.NewEncoder(file)
+	encoder.Indent("", "    ")
+	return encoder.Encode(data)
 }
