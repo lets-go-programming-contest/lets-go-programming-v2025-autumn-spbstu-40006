30b158ee4458c0b8de5bb67b4dcf4f609e8c8428
identicalaffiliation <fre1fsi.biz.c@gmail.com>
2025-10-31 19:49:17 +0300

    [TASK-3] tags json and xml fix

--- vladislav.mazitov/task-3/internal/currency/currency.go
+++ vladislav.mazitov/task-3/internal/currency/currency.go
@@ -10,13 +10,13 @@ import (
 type FloatType float64
 
 type Currency struct {
-	XMLName  xml.Name  `json:"-" xml:"Valute"`
-	ID       string    `json:"-" xml:"ID,attr"`
-	NumCode  int       `json:"num_code" xml:"NumCode"`
+	XMLName  xml.Name  `json:"-"         xml:"Valute"`
+	ID       string    `json:"-"         xml:"ID,attr"`
+	NumCode  int       `json:"num_code"  xml:"NumCode"`
 	CharCode string    `json:"char_code" xml:"CharCode"`
-	Nominal  int       `json:"-" xml:"Nominal"`
-	Name     string    `json:"-" xml:"Name"`
-	Value    FloatType `json:"value" xml:"Value"`
+	Nominal  int       `json:"-"         xml:"Nominal"`
+	Name     string    `json:"-"         xml:"Name"`
+	Value    FloatType `json:"value"     xml:"Value"`
 }
 
 type ValCurs struct {
