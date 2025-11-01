9a91cfbaa875ba0882dbcb11d9437c1e71418f2a
Mishaa105 <truhanov2006@mail.ru>
2025-11-01 18:06:15 +0300

    [TASK-3] fix

--- mikhail.trukhanov/task-3/internal/decoding/decoding.go
+++ mikhail.trukhanov/task-3/internal/decoding/decoding.go
@@ -30,7 +30,7 @@ func (f *FloatValue) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error 
 		return fmt.Errorf("decode element failed: %w", err)
 	}
 
-	valueStr = strings.Replace(v, ",", ".", 1)
+	valueStr = strings.Replace(valueStr, ",", ".", 1)
 	val, err := strconv.ParseFloat(valueStr, 64)
 	if err != nil {
 		return fmt.Errorf("cannot parse Value: %w", err)
