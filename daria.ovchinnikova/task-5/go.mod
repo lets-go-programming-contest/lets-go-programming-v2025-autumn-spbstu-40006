5e1c3e2afa5f2fba06359caf1b0b71e948400392
Овчинникова Дарья <daraartovskaa@gmail.com>
2025-11-30 17:13:09 +0300

    [TASK-5] Initial commit

---
+++ daria.ovchinnikova/task-5/go.mod
@@ -0,0 +1,3 @@
+module github.com/monka6/task-5
+
+go 1.22.7
---
+++ daria.ovchinnikova/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,121 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+const (
+	Prefix               = "decorated: "
+	NoDecoratorMessage   = "no decorator"
+	DecoratorError       = "can't be decorated"
+	NoMultiplexerMessage = "no multiplexer"
+	Undefined            = "undefined"
+)
+
+func PrefixDecoratorFunc(ctx context.Context, input chan string, output chan string) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, NoDecoratorMessage) {
+				return errors.New(DecoratorError)
+			}
+
+			if !strings.HasPrefix(data, Prefix) {
+				data = Prefix + data
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	defer func() {
+		for _, output := range outputs {
+			close(output)
+		}
+	}()
+
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	counter := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			outputIndex := counter % len(outputs)
+			counter++
+
+			select {
+			case outputs[outputIndex] <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	defer close(output)
+
+	if len(inputs) == 0 {
+		return nil
+	}
+
+	var waitGroup sync.WaitGroup
+
+	for _, inputChannel := range inputs {
+		waitGroup.Add(1)
+
+		go func(channel chan string) {
+			defer waitGroup.Done()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, ok := <-channel:
+					if !ok {
+						return
+					}
+
+					if strings.Contains(data, NoMultiplexerMessage) {
+						continue
+					}
+
+					select {
+					case output <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(inputChannel)
+	}
+
+	waitGroup.Wait()
+	return nil
+}
