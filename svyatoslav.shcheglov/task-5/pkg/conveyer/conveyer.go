55332b278eeb70f20b36c29d07803bfbe2b851b0
Svyatoslav Shcheglov <speaarit24@gmail.com>
2025-12-07 01:24:14 +0300

    [TASK-5] some fixes part 22

--- svyatoslav.shcheglov/task-5/pkg/conveyer/conveyer.go
+++ svyatoslav.shcheglov/task-5/pkg/conveyer/conveyer.go
@@ -6,8 +6,6 @@ import (
 	"sync"
 )
 
-const defaultChannelBufferSize = 16
-
 var ErrChanNotFound = errors.New("channel not found")
 
 type Conveyer struct {
@@ -27,11 +25,14 @@ func New() *Conveyer {
 func (c *Conveyer) obtainChannel(channelIdentifier string) chan string {
 	c.mutex.Lock()
 	channel, exists := c.channels[channelIdentifier]
-	if !exists {
-		channel = make(chan string, defaultChannelBufferSize)
-		c.channels[channelIdentifier] = channel
-	}
 	c.mutex.Unlock()
+	if !exists {
+		channel = make(chan string, 16)
+		c.mutex.Lock()
+		c.channels[channelIdentifier] = channel
+		c.mutex.Unlock()
+	}
+
 	return channel
 }
 
@@ -43,8 +44,10 @@ func (c *Conveyer) RegisterDecorator(
 	task := func(ctx context.Context) error {
 		sourceChannel := c.obtainChannel(sourceChannelID)
 		destinationChannel := c.obtainChannel(destinationChannelID)
+
 		return processor(ctx, sourceChannel, destinationChannel)
 	}
+
 	c.tasks = append(c.tasks, task)
 }
 
@@ -63,8 +66,10 @@ func (c *Conveyer) RegisterSeparator(
 		inputChannel := c.obtainChannel(inputChannelID)
 		outputTrueChannel := c.obtainChannel(outputTrueChannelID)
 		outputFalseChannel := c.obtainChannel(outputFalseChannelID)
+
 		return processor(ctx, inputChannel, outputTrueChannel, outputFalseChannel)
 	}
+
 	c.tasks = append(c.tasks, task)
 }
 
@@ -83,23 +88,29 @@ func (c *Conveyer) RegisterMultiplexer(
 		leftInputChannel := c.obtainChannel(leftInputChannelID)
 		rightInputChannel := c.obtainChannel(rightInputChannelID)
 		outputChannel := c.obtainChannel(outputChannelID)
+
 		return processor(ctx, leftInputChannel, rightInputChannel, outputChannel)
 	}
+
 	c.tasks = append(c.tasks, task)
 }
 
 func (c *Conveyer) Send(channelID string, value string) error {
 	targetChannel := c.obtainChannel(channelID)
+
 	targetChannel <- value
+
 	return nil
 }
 
 func (c *Conveyer) Recv(channelID string) (string, error) {
 	targetChannel := c.obtainChannel(channelID)
 	value, ok := <-targetChannel
+
 	if !ok {
 		return "", nil
 	}
+
 	return value, nil
 }
 
@@ -110,12 +121,15 @@ func (c *Conveyer) Run(ctx context.Context) error {
 	for _, task := range c.tasks {
 		taskGroup.Add(1)
 		currentTask := task
-		go func() {
+
+		go func(taskFunc func(ctx context.Context) error) {
 			defer taskGroup.Done()
-			if err := currentTask(ctx); err != nil {
+
+			err := taskFunc(ctx)
+			if err != nil {
 				errorsChannel <- err
 			}
-		}()
+		}(currentTask)
 	}
 
 	taskGroup.Wait()
