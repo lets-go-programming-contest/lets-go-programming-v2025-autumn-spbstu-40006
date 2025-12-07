772cb37618e0a33a3755d41deba9f7a736131413
Mishaa105 <truhanov2006@mail.ru>
2025-12-07 22:16:04 +0300

    [TASK-5] (#129)
    * [TASK-5] Initial commit
    
    * [TASK-5] go.sum added
    
    * [TASK-5] fix
    
    * [TASK-5] run fix
    
    * [TASK-5] run fix 2
    
    * [TASK-5] run fix 3
    
    * [TASK-5] fix
    
    * [TASK-5] another fix
    
    * [TASK-5] handlers fix
    
    * [TASK-5] fix
    
    * [TASK-5] send and recv fix
    
    * [TASK-5] fix
    
    * [TASK-5] conveyer fix
    
    * [TASK-5] lint fix
    
    * [TASK-5] final fix

---
+++ mikhail.trukhanov/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,112 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var ErrCannotBeDecorated = errors.New("can't be decorated")
+
+func PrefixDecoratorFunc(ctx context.Context, input, output chan string) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case val, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(val, "no decorator") {
+				return ErrCannotBeDecorated
+			}
+
+			if !strings.HasPrefix(val, "decorated: ") {
+				val = "decorated: " + val
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case output <- val:
+			}
+		}
+	}
+}
+
+func SeparatorFunc(ctx context.Context, input chan string, outputs []chan string) error {
+	defer func() {
+		for _, ch := range outputs {
+			close(ch)
+		}
+	}()
+
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	idx := 0
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case val, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case <-ctx.Done():
+				return nil
+			case outputs[idx%len(outputs)] <- val:
+				idx++
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inputs []chan string, output chan string) error {
+	defer close(output)
+
+	waitGroup := sync.WaitGroup{}
+
+	for _, ch := range inputs {
+		waitGroup.Add(1)
+
+		channel := ch
+
+		go func() {
+			defer waitGroup.Done()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case val, ok := <-channel:
+					if !ok {
+						return
+					}
+
+					if strings.Contains(val, "no multiplexer") {
+						continue
+					}
+
+					select {
+					case <-ctx.Done():
+						return
+					case output <- val:
+					}
+				}
+			}
+		}()
+	}
+
+	waitGroup.Wait()
+
+	return nil
+}
