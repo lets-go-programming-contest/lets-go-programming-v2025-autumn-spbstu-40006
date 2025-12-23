168b428593f87749bb1f315f04e452d6454a86f6
Дарья <shyriaevaea@gmail.com>
2025-12-23 23:58:55 +0300

    [TASK-8] Initial commit

---
+++ shiryaeva.daria/task-8/cmd/service/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sp3c7r/task-8/internal/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "load config error: %v", err)
+		return
+	}
+
+	fmt.Print(cfg.Environment, " ", cfg.LogLevel)
+}
