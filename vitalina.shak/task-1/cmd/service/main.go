af7eeb1faa6e100c4f4c6a0cacd4da541502236d
vitsh1 <vips65653@gmail.com>
2025-12-28 14:08:16 +0300

    [TASK-1] add code

--- vitalina.shak/task-1/cmd/service/main.go
+++ vitalina.shak/task-1/cmd/service/main.go
@@ -3,5 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Task 1")
+
+	var first int
+	var second int
+	var operator string
+
+	if _, err := fmt.Scanln(&first); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	if _, err := fmt.Scanln(&second); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	if _, err := fmt.Scanln(&operator); err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operator {
+
+	case "+":
+		fmt.Println(first + second)
+	case "-":
+		fmt.Println(first - second)
+	case "*":
+		fmt.Println(first * second)
+	case "/":
+
+		if second == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+
+		fmt.Println(first / second)
+
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
