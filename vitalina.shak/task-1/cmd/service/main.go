48793f6b179dc795ce508311e0aacc3eb1d9c731
vitsh1 <vips65653@gmail.com>
2025-12-28 14:13:43 +0300

    [TASK-1] fix spaces

--- vitalina.shak/task-1/cmd/service/main.go
+++ vitalina.shak/task-1/cmd/service/main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
 	var first int
 	var second int
 	var operator string
@@ -24,7 +23,6 @@ func main() {
 	}
 
 	switch operator {
-
 	case "+":
 		fmt.Println(first + second)
 	case "-":
@@ -32,14 +30,11 @@ func main() {
 	case "*":
 		fmt.Println(first * second)
 	case "/":
-
 		if second == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
-
 		fmt.Println(first / second)
-
 	default:
 		fmt.Println("Invalid operation")
 	}
