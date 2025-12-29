e3c828b8920bbd64b6e6f8d94f5a1dbb96818282
vitsh1 <vips65653@gmail.com>
2025-12-29 12:48:28 +0300

    [TASK-2-1] add code

--- vitalina.shak/task-2-1/cmd/service/main.go
+++ vitalina.shak/task-2-1/cmd/service/main.go
@@ -1,6 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	initMinTemp = 15
+	initMaxTemp = 30
+)
 
 func main() {
+	var departmentsCount int
+	if _, err := fmt.Scan(&departmentsCount); err != nil {
+		fmt.Println("Failed to read departments count")
+		return
+	}
+
+	for departmentIndex := 0; departmentIndex < departmentsCount; departmentIndex++ {
+		if err := processDepartment(); err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+}
+
+func processDepartment() error {
+	var employeesCount int
+	if _, err := fmt.Scan(&employeesCount); err != nil {
+		return errors.New("Failed to read employees count")
+	}
+
+	currentMinTemp := initMinTemp
+	currentMaxTemp := initMaxTemp
+
+	for employeeIndex := 0; employeeIndex < employeesCount; employeeIndex++ {
+		var operation string
+		var temp int
+
+		if _, err := fmt.Scan(&operation, &temp); err != nil {
+			return errors.New("Failed to read requirement")
+		}
+
+		switch operation {
+		case ">=":
+			if temp > currentMinTemp {
+				currentMinTemp = temp
+			}
+		case "<=":
+			if temp < currentMaxTemp {
+				currentMaxTemp = temp
+			}
+		default:
+			fmt.Println("Invalid operation")
+			continue
+		}
+
+		if currentMinTemp <= currentMaxTemp {
+			fmt.Println(currentMinTemp)
+		} else {
+			fmt.Println(-1)
+		}
+	}
+
+	return nil
 }
