e43c64e99ae442646b108a2d6c116dfd09ca7b2d
Глинский Илья <glinckyweb@gmail.com>
2026-01-06 19:57:23 +0300

    [TASK-5] the beginnings

--- ilya.glinskiy/task-5/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/bloomkicks/task-5
-
-go 1.22.7
---
+++ ilya.glinskiy/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,36 @@
+package conveyer
+
+import "context"
+
+type conveyer interface {
+	RegisterDecorator(
+		fn func(
+			ctx context.Context,
+			input chan string,
+			output chan string,
+		) error,
+		input string,
+		output string,
+	)
+	RegisterMultiplexer(
+		fn func(
+			ctx context.Context,
+			inputs []chan string,
+			output chan string,
+		) error,
+		inputs []string,
+		output string,
+	)
+	RegisterSeparator(
+		fn func(
+			ctx context.Context,
+			input chan string,
+			outputs []chan string,
+		) error,
+		input string,
+		outputs []string,
+	)
+	Run(ctx context.Context) error
+	Send(input string, data string) error
+	Recv(output string) (string, error)
+}
