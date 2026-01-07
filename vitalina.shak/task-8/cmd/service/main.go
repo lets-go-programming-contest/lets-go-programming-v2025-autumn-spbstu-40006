f04fdbdd10f8ddd4658cfb26c3d3844982c8c2d8
vitsh1 <vips65653@gmail.com>
2026-01-07 18:20:56 +0300

    [TASK-8] add code

--- vitalina.shak/task-8/cmd/service/main.go
+++ vitalina.shak/task-8/cmd/service/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/vitsh1/task-8/config"
+)
+
 func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Println("failed to load config: %w", err)
+
+		return
+	}
+
+	fmt.Println(cfg.Environment, cfg.LogLevel)
 }
