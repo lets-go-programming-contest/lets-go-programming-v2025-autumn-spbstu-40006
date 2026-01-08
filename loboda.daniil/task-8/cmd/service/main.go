de9c47c459e20edc853a1f6327ba897b6757b266
DaniilNotResponding <gohankobol@gmail.com>
2026-01-09 00:35:45 +0300

    [TASK-8] X2

---
+++ loboda.daniil/task-8/cmd/service/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Daniil-drom/task-8/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
+}
