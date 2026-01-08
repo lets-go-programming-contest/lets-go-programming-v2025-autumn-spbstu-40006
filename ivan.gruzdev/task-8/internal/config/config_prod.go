ff211021f48811823824834740e03515a72b9a15
Ivan Gruzdev <dakjacquesnoname@gmail.com>
2026-01-08 18:28:51 +0300

    [TASK-8] (#191)
    * [TASK-8] inintial commit
    
    * [TASK-8] fix test
    
    * [TASK-8] fix lint

---
+++ ivan.gruzdev/task-8/internal/config/config_prod.go
@@ -0,0 +1,24 @@
+//go:build !dev
+
+package config
+
+import (
+	_ "embed"
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed prod.yaml
+var prodConfig []byte
+
+func GetConfig() (*Config, error) {
+	var cfg Config
+
+	err := yaml.Unmarshal(prodConfig, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка чтения конфигурации: %w", err)
+	}
+
+	return &cfg, nil
+}
