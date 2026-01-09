601868aa609c3f6ea7a1d24a99421a282b7e5542
DaniilNotResponding <gohankobol@gmail.com>
2026-01-09 08:44:14 +0300

    [TASK-8] (#203)
    * [TASK-8] X2
    
    * [TASK-8] X3
    
    * [TASK-8] X4 atmp

---
+++ loboda.daniil/task-8/cmd/service/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Daniil-drom/task-8/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s %s", cfg.Environment, cfg.LogLevel)
+}
---
+++ loboda.daniil/task-8/config/config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func Load() (Config, error) {
+	var cfg Config
+	if err := yaml.Unmarshal(rawConfigYAML, &cfg); err != nil {
+		return Config{}, fmt.Errorf("parse embedded config yaml: %w", err)
+	}
+
+	return cfg, nil
+}
